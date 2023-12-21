643714c1b37d1035c9d4dda4541e7300bbeae356
angeltried <angeltried@icloud.com>
2023-12-20 21:24:37 -0600

    card

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ window.onload = function(){
     document.getElementById("fadein").remove();
   },1000);
 };
+
+const card = document.querySelector(".card__inner");
+
+card.addEventListener("click", function (e) {
+  card.classList.toggle('is-flipped');
+});
