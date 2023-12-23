8946df300a866c5b5c32edc569cb2cdf1f8a5207
angeltried <angeltried@icloud.com>
2023-12-23 11:44:45 -0600

    funtions

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ window.onload = function(){
   },1000);
 };
 
+function showSection() {
+  var i;
+  for (i = 0; i < links.length; i += 1) {
+      document.getElementById(links[i].href.split('#')[1]).className = 'hidden';
+  }
+  document.getElementById(this.href.split('#')[1]).className = '';
+  return false;
+}
+function createShowSection(links) {
+  return showSection;
+}
+var sections = document.getElementById('sections'),
+  links = sections.getElementsByTagName('a'),
+  showSection = createShowSection(links),
+  i;
+for (i = 0; i < links.length; i += 1) {
+  links[i].onclick = showSection;
+}
+showSection();
+
+
+
 // const card = document.querySelector(".card__inner");
 
 // card.addEventListener("click", function (e) {
@@ -11,4 +33,25 @@ window.onload = function(){
 // });
 
 
+// var topShelf = document.getElementById("topShelf");
+// var flower = document.getElementById("flower");
+// var concentrates = document.getElementById("concentrate");
+
+// function myShelf () {
+//  if (topShelf.style.display === "none") {
+//   topShelf.style.display = "block";
+//  } else {
+//   flower.style.display ="none";
+//   concentrates.style.display ="none";
+//  }
+  
+// }
+// function myFlower () {
+
+  
+// }
+// function myConcentrate () {
+
+  
+// }
 
